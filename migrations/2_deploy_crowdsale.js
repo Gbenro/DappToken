4e70d98d8d82745fe01695c57aa889049d00b714
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-10 21:39:14 -0600

    set up coin

---
+++ migrations/2_deploy_crowdsale.js
@@ -0,0 +1,8 @@
+var DappToken = artifacts.require('./DappToken.sol')
+
+module.exports = function (deployer) {
+  const _name = 'Dapp Token'
+  const _symbol = 'DAPP'
+  const _decimals = 18
+  deployer.deploy(DappToken, _name, _symbol, _decimals)
+}
